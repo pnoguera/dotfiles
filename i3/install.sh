ebda8f99c597143e490f8f7e31f46f200965052d
Pablo Noguera <pablo.noguera@flixbus.com>
2019-02-22 14:09:33 +0100

    Add i3lock before sleep

--- i3/install.sh
+++ i3/install.sh
@@ -2,3 +2,8 @@
 
 # Install font required for the status bar
 sudo apt-get install fonts-font-awesome
+
+# Configure i3lock before suspend
+[ -d ~/.config/systemd ] && echo "Deleting .config/systemd" && rm -rf ~/.config/systemd
+ln -s $PWD/systemd ~/.config/
+systemctl --user enable i3lock
