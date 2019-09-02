6913abce39667c46d12025c03576054485bcbcc5
Pablo Noguera <pablo.noguera@flixbus.com>
2019-09-02 13:48:46 +0200

    Add black python formatter

--- script/utilities.sh
+++ script/utilities.sh
@@ -4,3 +4,6 @@ which go || (echo "Go isi either not installed or not in \$PATH" && exit 2)
 
 echo "Installing pt"
 go get -u github.com/monochromegane/the_platinum_searcher/...
+
+echo "Install black python formatter"
+pip3 install black
