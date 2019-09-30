43aa6e5c8bfb12f9b9db5af185ffc1ebfd44a263
Pablo Noguera <pablo.noguera@flixbus.com>
2019-09-30 13:53:46 +0200

    Add yagostatus+conky bar

--- i3/install.sh
+++ i3/install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # Install font required for the status bar
-sudo apt-get install fonts-font-awesome
+sudo apt-get install fonts-font-awesome conky
+
+# yagostatus
+echo "Installing yagostatus bar"
+go get github.com/burik666/yagostatus
 
 # Configure i3lock before suspend
 [ -d ~/.config/systemd ] && echo "Deleting .config/systemd" && rm -rf ~/.config/systemd
