3dc9e8338d46e302e5b817e37c7adccc387c1ccf
Pablo Noguera <pablo.noguera@flixbus.com>
2019-11-14 15:24:35 +0100

    Add autodisplay

--- i3/install.sh
+++ i3/install.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 # Install font required for the status bar
-sudo apt-get install fonts-font-awesome conky
+sudo apt-get -y install fonts-font-awesome conky
 
 # yagostatus
 echo "Installing yagostatus bar"
 go get github.com/burik666/yagostatus
+
+# autodisplay
+echo "Installing autodisplay"
+apt-get -y install autorandr
+# curl -L https://github.com/lpicanco/i3-autodisplay/releases/download/v0.2/i3-autodisplay-0.2-linux-amd64 -o ~/bin/i3-autodisplay
+# chmod +x ~/bin/i3-autodisplay
 
 # Configure i3lock before suspend
 [ -d ~/.config/systemd ] && echo "Deleting .config/systemd" && rm -rf ~/.config/systemd
