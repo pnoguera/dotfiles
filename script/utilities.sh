f558baabaa986bb926e8572c3d77e17c99d3648d
Pablo Noguera <pablo.noguera@flixbus.com>
2020-01-17 15:07:55 +0100

    Add bash-my-aws

--- script/utilities.sh
+++ script/utilities.sh
@@ -7,3 +7,6 @@ go get -u github.com/monochromegane/the_platinum_searcher/...
 
 echo "Install black python formatter"
 pip3 install black
+
+echo "Install Bash-My-AWS"
+git clone https://github.com/bash-my-aws/bash-my-aws.git ~/.bash-my-aws
