b7e9c1e860e284343b2ebd3ffb454920addc2671
Pablo Noguera <pablo.noguera@flixbus.com>
2020-05-24 23:02:55 +0200

    Fix vim install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
-
 
 if [ -f /etc/debian_version ]; then
     sudo apt-get -y install \
         vim \
         vim-gnome \
-        python-requests \
         markdown \
         xdotool
 fi
+
+go get -u github.com/cweill/gotests/...
