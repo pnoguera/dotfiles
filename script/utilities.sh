c0bfe1b3a8677115f46c6a0c53d1b0f87d3648fc
Pablo Noguera <pablo.noguera@flixbus.com>
2020-06-17 11:24:32 +0200

    Add direnv

--- script/utilities.sh
+++ script/utilities.sh
@@ -10,3 +10,7 @@ pip3 install black
 
 echo "Install Bash-My-AWS"
 git clone https://github.com/bash-my-aws/bash-my-aws.git ~/.bash-my-aws
+
+echo "Install various utilities"
+apt-get update
+apt-get -y install direnv
