65d9e30fc6a8e1fa642cc585fdda8d4b220df15d
Pablo Noguera <pablo@noguera.com.es>
2023-01-09 23:14:00 +0100

    Improve installation

--- script/utilities.sh
+++ script/utilities.sh
@@ -3,7 +3,7 @@
 which go || (echo "Go isi either not installed or not in \$PATH" && exit 2)
 
 echo "Installing pt"
-go get -u github.com/monochromegane/the_platinum_searcher/...
+curl -L https://github.com/monochromegane/the_platinum_searcher/releases/download/v2.2.0/pt_linux_amd64.tar.gz|tar xvz -C ~/bin/ --strip 1 pt_linux_amd64/pt
 
 echo "Install black python formatter"
 pip3 install black
@@ -12,5 +12,9 @@ echo "Install Bash-My-AWS"
 git clone https://github.com/bash-my-aws/bash-my-aws.git ~/.bash-my-aws
 
 echo "Install various utilities"
-apt-get update
-apt-get -y install direnv
+sudo apt-get update
+sudo apt-get -y install direnv
+
+echo "Install git-prompt"
+mkdir .bash-git-prompt
+curl gitprompt.sh -o ~/.bash-git-prompt/gitprompt.sh
