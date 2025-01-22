f87e459a17a1b06136510ef9a79394ce13ac4794
Pablo Noguera <pablo.noguera@flixbus.com>
2025-01-22 14:41:31 +0100

    Add neovim config

---
+++ config/nvim.symlink/init.vim
@@ -0,0 +1,4 @@
+lua require('init')
+" set runtimepath^=~/.vim runtimepath+=~/.vim/after
+" let &packpath = &runtimepath
+" source ~/.vimrc
---
+++ config/nvim.symlink/lua/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+return{
+    {
+        "wilmanbarrios/palenight.nvim",
+        lazy = false,
+        config = function()
+            require("palenight").set()
+        end,
+    }
+}
