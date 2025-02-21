a96e17f490f08d804b65c76991a3fafb1cf63d89
Pablo Noguera <pablo.noguera@flixbus.com>
2025-02-21 15:37:10 +0100

    Add markdown links keybinding

--- config/nvim.symlink/lua/config/keymaps.lua
+++ config/nvim.symlink/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+
+-- Keymap for Markdown links
+vim.keymap.set("v", "<leader>ml", 'c[<C-r>"]()<Esc>', { desc = "[P]Markdown: insert link)" })
+vim.keymap.set("n", "<leader>ml", 'ciw[<C-r>"]()<Esc>', { desc = "[P]Markdown: insert link)" })
+vim.keymap.set("v", "<leader>mk", 'c[<C-r>"](<Esc>"+pli)<Esc>', { desc = "[P]Markdown: insert link from clipboard)" })
+vim.keymap.set("n", "<leader>mk", 'ciw[<C-r>"](<Esc>"+pli)<Esc>', { desc = "[P]Markdown: insert link from clipboard)" })
