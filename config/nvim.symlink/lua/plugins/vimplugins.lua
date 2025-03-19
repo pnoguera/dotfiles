b53cf044a9b351ba13d6e5282f9f4dc8f2fa5bc3
Pablo Noguera <pablo.noguera@flixbus.com>
2025-03-19 10:04:26 +0100

    Add dispatch plugins

--- config/nvim.symlink/lua/plugins/vimplugins.lua
+++ config/nvim.symlink/lua/plugins/vimplugins.lua
@@ -1,5 +1,7 @@
 return {
 	"tpope/vim-fugitive", -- git plugin
+	"tpope/vim-dispatch",
+	"radenling/vim-dispatch-neovim",
 	"airblade/vim-gitgutter", -- shows git diff in the gutter
 	-- "github/copilot.vim", -- Copilot plugin
 }
