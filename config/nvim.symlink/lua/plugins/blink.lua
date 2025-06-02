86f300bfa73372d875ad0e668630a1d74f9bc4f6
Pablo Noguera <pablo.noguera@flixbus.com>
2025-06-02 14:21:27 +0200

    Disable autocomplete on markdown

--- config/nvim.symlink/lua/plugins/blink.lua
+++ config/nvim.symlink/lua/plugins/blink.lua
@@ -1,20 +1,55 @@
 return {
-	'saghen/blink.cmp',
+	"saghen/blink.cmp",
 	opts = {
+		-- Exceptions: vim.bo.filetype == 'dap-repl'
+		enabled = function()
+			return not vim.tbl_contains({ "lua", "markdown" }, vim.bo.filetype)
+		end,
+
 		sources = {
-			 -- Add 'dictionary' to the list
-			-- default = function(ctx)
-			--   local success, node = pcall(vim.treesitter.get_node)
-			--   if vim.bo.filetype == 'markdown' then
-			-- 	return { 'dictionary', 'lsp', 'path' }
-			--   elseif success and node and vim.tbl_contains({ 'comment', 'line_comment', 'block_comment' }, node:type()) then
-			-- 	return { 'buffer' }
-			--   else
-			-- 	return { 'dictionary', 'lsp', 'path', 'snippets', 'buffer' }
-			--   end
-			-- end,
-			-- default = { 'dictionary', 'lsp', 'path', 'snippets', 'buffer' }
+			-- Remove 'buffer' if you don't want text completions, by default it's only enabled when LSP returns no items
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+		completion = {
+			-- 'prefix' will fuzzy match on the text before the cursor
+			-- 'full' will fuzzy match on the text before _and_ after the cursor
+			-- example: 'foo_|_bar' will match 'foo_' for 'prefix' and 'foo__bar' for 'full'
+			keyword = { range = "full" },
 			min_keyword_length = 5,
-		}
-	}
+
+			-- Disable auto brackets
+			-- NOTE: some LSPs may add auto brackets themselves anyway
+			accept = { auto_brackets = { enabled = true } },
+
+			-- Don't select by default, auto insert on selection
+			list = { selection = { preselect = false, auto_insert = true } },
+			-- or set via a function
+			list = {
+				selection = {
+					preselect = function(ctx)
+						return vim.bo.filetype ~= "markdown"
+					end,
+				},
+			},
+
+			menu = {
+				-- Don't automatically show the completion menu
+				auto_show = false,
+
+				-- nvim-cmp style menu
+				draw = {
+					columns = {
+						{ "label", "label_description", gap = 1 },
+						{ "kind_icon", "kind" },
+					},
+				},
+			},
+
+			-- Show documentation when selecting a completion item
+			documentation = { auto_show = true, auto_show_delay_ms = 500 },
+
+			-- Display a preview of the selected item on the current line
+			ghost_text = { enabled = true },
+		},
+	},
 }
